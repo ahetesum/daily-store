b3294f6c5a02af2622a348e4019a5dd3358a2558
hb <hb@hbs-MacBook-Pro-3.local>
2022-04-26 20:38:18 +0530

    slider completed

---
+++ src/Components/Func/CartComponent.js
@@ -0,0 +1,9 @@
+
+
+export const CartComponent=()=>{
+    return(
+        <div>
+            <img  />
+        </div>
+    )
+}
--- src/Components/Func/DailyStoreHeader.js
+++ src/Components/Func/DailyStoreHeader.js
@@ -4,5 +4,6 @@ import logo from '../../Assets/logo.png'
 export const DailyStoreHeader =()=>{
     return <div className="headerContainer">
         <img src={logo} className='logoImg' />
+        
     </div>
 }
