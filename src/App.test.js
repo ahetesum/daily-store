1ec18e8068e821cc558db83bf37a359db00612eb
VITHIKA BHATT <vithikabhatt1999@gmail.com>
2022-05-04 21:19:34 +0530

    commit

--- src/App.test.js
+++ src/App.test.js
@@ -6,4 +6,4 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
-//My name is Vithika Bhatt my friend Name is Soumya. We are fron SBU
+//My name is Vithika Bhatt my friend Name is Soumya. We pursue Electronic and Communication. We are from SBU 
